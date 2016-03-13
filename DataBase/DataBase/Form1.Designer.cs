4dfb3a63f8b27902b03d273e976a7df30dd325d8
VladTatarenko <vldtat1995@bigmir.net>
2016-03-13 23:45:02 +0200

    Останні зміни

--- DataBase/DataBase/Form1.Designer.cs
+++ DataBase/DataBase/Form1.Designer.cs
@@ -108,7 +108,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Begin form";
+            this.Text = "Початкова форма";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
