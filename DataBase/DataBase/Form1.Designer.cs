61ad203dd60dfb6b07d7b0268d79f118e856c151
VladTatarenko <vldtat1995@bigmir.net>
2016-03-13 23:26:05 +0200

    Face commit

--- DataBase/DataBase/Form1.Designer.cs
+++ DataBase/DataBase/Form1.Designer.cs
@@ -41,7 +41,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.LightSalmon;
             this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(416, 225);
+            this.button1.Location = new System.Drawing.Point(427, 225);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 1;
